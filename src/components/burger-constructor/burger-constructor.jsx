929e597d01950a23aaed7761c8be30c581f8bacd
yaroslav em <mr.emelyanenko@gmail.com>
2023-12-05 18:26:23 +0100

    Isolating checkResponce function in separate file

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -13,19 +13,18 @@ import {
 import BurgerIngredient from '../burger-ingredient/burger-ingredient';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { setBunTop, setBunBottom, setIngredients } from '../../services/burger-constructor/burger-constructor-slice';
-import { selectBunTop, selectBunBottom, selectIngredients } from '../../services/burger-constructor/selectors';
+import { setBun, setIngredients } from '../../services/burger-constructor/burger-constructor-slice';
+import { selectBun, selectIngredients } from '../../services/burger-constructor/selectors';
 
 function BurgerConstructor() {
   const [isModalOpen, setModalOpen] = useState(false);
   const [totalPrice, setTotalPrice] = useState(0);
   const dispatch = useDispatch();
-  // const bunTop = useSelector(selectBunTop);
-  // const bunBottom = useSelector(selectBunBottom);
+  // const bun = useSelector(selectBun);
   // const ingredients = useSelector(selectIngredients);
 
 
-  const handleOrderClick = () => {
+  const openModal = () => {
     setModalOpen(true);
   };
 
@@ -49,35 +48,35 @@ function BurgerConstructor() {
   return (
     <>
       <section className={`${burgerConstructor.container} pt-25 pl-4 pr-4`}>
-      {/* <div className={`${burgerConstructor.top} mb-4 mr-4`}>
-      {bunTop && (
+      <div className={`${burgerConstructor.top} mb-4 mr-4`}>
+      {/* {bun && (
             <ConstructorElement
               type="top"
               isLocked={true}
-              text={`${bunTop.name} (верх)`}
-              price={bunTop.price}
-              thumbnail={bunTop.image}
+              text={`${bun.name} (верх)`}
+              price={bun.price}
+              thumbnail={bun.image}
             />
-          )}
+          )} */}
         </div>
         <ul className={`${burgerConstructor.list} custom-scroll`}>
-          {nonBunIngredients.map((item, index) => (
+          {/* {nonBunIngredients.map((item, index) => (
             <li className={`${burgerConstructor.item} mb-4 mr-2`} key={index}>
              <BurgerIngredient key={index} item={item} />
             </li>
-          ))}
+          ))} */}
         </ul>
         <div className={`${burgerConstructor.botom} mt-4 mr-4`}>
-        {bunBottom && (
+        {/* {bun && (
             <ConstructorElement
               type="bottom"
               isLocked={true}
-              text={`${bunBottom.name} (низ)`}
-              price={bunBottom.price}
-              thumbnail={bunBottom.image}
+              text={`${bun.name} (низ)`}
+              price={bun.price}
+              thumbnail={bun.image}
             />
-          )}
-        </div> */}
+          )} */}
+        </div>
         <div className={`${burgerConstructor.total} mt-10`}>
           <p className='text text_type_digits-medium mr-10'>
           {totalPrice} <CurrencyIcon type='primary' />
@@ -86,7 +85,7 @@ function BurgerConstructor() {
             htmlType='button'
             type='primary'
             size='medium'
-            onClick={handleOrderClick}
+            onClick={openModal}
           >
             Оформить заказ
           </Button>
--- src/utils/burger-api.js
+++ src/utils/burger-api.js
@@ -1,10 +1,8 @@
+import { checkResponse } from './check-response';
+
 export const BURGER_API_URL = 'https://norma.nomoreparties.space/api';
 
 export const getIngredients = async () => {
     const res = await fetch(`${BURGER_API_URL}/ingredients`);
     return checkResponse(res);
-};
-
-export const checkResponse = (res) => {
-    return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 };
